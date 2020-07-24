b5fe2cf748df6d8d351eb6423a3a9a7f476b8006
Tanvir Ahmod <tanvirahmodcse@gmail.com>
2020-07-24 11:54:48 +0600

    Detect text from canvas

--- lib/ui/canvas.dart
+++ lib/ui/canvas.dart
@@ -1,10 +1,15 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:dictionary/ui/background.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_signature_pad/flutter_signature_pad.dart';
 import 'dart:ui' as ui;
 
 import 'package:hexcolor/hexcolor.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CustomCanvas extends StatefulWidget {
   @override
@@ -14,7 +19,7 @@ class CustomCanvas extends StatefulWidget {
 class _CustomCanvasState extends State<CustomCanvas> {
   ByteData _img = ByteData(0);
 
-  var color = Colors.black54;
+  var color = Colors.blueAccent;
 
   var strokeWidth = 5.0;
 
@@ -71,10 +76,6 @@ class _CustomCanvasState extends State<CustomCanvas> {
           child: Signature(
             key: _sign,
             color: color,
-            /*onSign: () {
-                    final sign = _sign.currentState;
-                    debugPrint('${sign.points.length} points in the signature');
-                  },*/
             backgroundPainter: _WatermarkPaint("2.0", "2.0"),
             strokeWidth: strokeWidth,
           ),
@@ -90,12 +91,19 @@ class _CustomCanvasState extends State<CustomCanvas> {
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             IconButton(
-              icon: Icon(Icons.search, size: 40, color: Hexcolor('3f51b5')),
-            ),
+                icon: Icon(Icons.search, size: 40, color: Hexcolor('3f51b5')),
+                onPressed: () async {
+                  _detectText();
+                }),
             SizedBox(width: 10),
             IconButton(
               icon: Icon(Icons.clear, size: 40, color: Hexcolor('3f51b5')),
-              onPressed: () => _sign.currentState.clear(),
+              onPressed: () {
+                _sign.currentState.clear();
+                setState(() {
+                  _img = ByteData(0);
+                });
+              },
             ),
             SizedBox(width: 40),
           ],
@@ -103,8 +111,38 @@ class _CustomCanvasState extends State<CustomCanvas> {
         SizedBox(
           height: 5,
         ),
+
       ]),
     );
+  }
+
+  void _detectText() async {
+    final sign = _sign.currentState;
+    final image = await sign.getData();
+    var data = await image.toByteData(format: ui.ImageByteFormat.png);
+
+    setState(() {
+      _img = data;
+    });
+
+    // Temporary saving file
+    final directory = await getApplicationDocumentsDirectory();
+    final imageFile = File('${directory.path}/my_file.jpg');
+    await imageFile.writeAsBytes(
+        data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes));
+    FirebaseVisionImage firebaseVisionImage =
+        FirebaseVisionImage.fromFile(imageFile);
+
+    TextRecognizer textRecogniser = FirebaseVision.instance.textRecognizer();
+    VisionText visionText =
+        await textRecogniser.processImage(firebaseVisionImage);
+    for (TextBlock textBlock in visionText.blocks) {
+      for (TextLine line in textBlock.lines) {
+        for (TextElement word in line.elements) {
+          print('word ${word.text}');
+        }
+      }
+    }
   }
 }
 
@@ -116,8 +154,7 @@ class _WatermarkPaint extends CustomPainter {
 
   @override
   void paint(ui.Canvas canvas, ui.Size size) {
-    /* canvas.drawCircle(Offset(size.width / 2, size.height / 2), 10.8,
-        Paint()..color = Colors.blue);*/
+
   }
 
   @override
